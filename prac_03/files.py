77829e840bd747909aa74a1d2d59b63494fb0817
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-09-29 16:28:13 +1000

    Add program 4 of Files

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,20 +1,20 @@
 """ 1. Write code that asks the user for their name, then opens a file called name.txt and writes that name to it.
     Use open and close for this question. """
 
-out_file = open("name.txt", "w")
-name = input("Enter your name: ")
-out_file.write(name)
-out_file.close()
+# out_file = open("name.txt", "w")
+# name = input("Enter your name: ")
+# out_file.write(name)
+# out_file.close()
 
 """ 2. In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name
     (as above) then prints (note the exact output),
     Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
     Use open and close for this question. """
 
-in_file = open("name.txt", "r")
-name = in_file.read().strip()
-in_file.close()
-print(f"Hi {name}!")
+# in_file = open("name.txt", "r")
+# name = in_file.read().strip()
+# in_file.close()
+# print(f"Hi {name}!")
 
 """ Create a text file called numbers.txt and save it in your prac directory. Put the following three numbers on
     separate lines in the file and save it:
@@ -28,4 +28,13 @@ with open("numbers.txt", "r") as in_file:
     first_number = int(in_file.readline())
     second_number = int(in_file.readline())
     result = first_number + second_number
-    print(result)
+print(result)
+
+""" 4. Now write a fourth block of code that prints the total for all lines in numbers.txt.
+    This should work for a file with any number of numbers. Use with instead of open and close for this question."""
+
+with open("numbers.txt", "r") as in_file:
+    total = 0
+    for line in in_file:
+        total += int(line)
+print(total)
