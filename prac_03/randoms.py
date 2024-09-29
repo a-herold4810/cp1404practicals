28ad61beb5ed83ea04f364c43f83fd0e5d294bf7
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-09-29 12:08:40 +1000

    Add questions, answers and program to produce a random number between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,23 @@
+""" What did you see on line 1?
+What was the smallest number you could have seen, what was the largest? """
+# prints a random interger between 5 and 20
+# Smallest - 5
+# Largest - 20
+
+""" What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4? """
+# Prints a random number out of the options of [3,5,7,9]. As it is looking in the range of 3-10 in steps of 2.
+# Smallest - 3
+# Largest - 9
+# No
+
+""" What did you see on line 3?
+What was the smallest number you could have seen, what was the largest? """
+# produces a float between 2.5 and 5.5
+# Smallest - 2.5
+# Largest - 5.5
+
+import random
+
+print(random.randint(1, 100))
