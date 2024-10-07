532e6cca226ef658eacc11a58b7a181384456422
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-10-07 12:33:50 +1000

    Add warm-up questions and answers

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+""" 1. numbers[0]
+    2. numbers[-1]
+    3. numbers[3]
+    4. numbers[:-1]
+    5. numbers[3:4]
+    6. 5 in numbers
+    7. 7 in numbers
+    8. "3" in numbers
+    9. numbers + [6, 5, 3] """
+
+# 1. 3
+# 2. 2
+# 3. 1
+# 4. [3, 1, 4, 1, 5, 9]
+# 5. [1]
+# 6. True
+# 7. False
+# 8. False
+# 9. [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
