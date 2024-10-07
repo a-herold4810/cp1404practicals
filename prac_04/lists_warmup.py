2583549f0924ba92ee314c642e7ac51100c539ea
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-10-07 12:38:26 +1000

    Add additional warm-up questions and answers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,3 +1,4 @@
+
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
 """ 1. numbers[0]
@@ -19,3 +20,20 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7. False
 # 8. False
 # 9. [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+""" 1. Change the first element of numbers to "ten" (the string, not the number 10)
+    2. Change the last element of numbers to 1
+    3. Print all the elements from numbers except the first two (slice)
+    4. Print whether 9 is an element of numbers """
+
+# 1.
+numbers[0] = "ten"
+
+# 2.
+numbers[-1] = 1
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
