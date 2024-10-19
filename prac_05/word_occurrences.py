ce593c1e3ac38e57e4592da302abee9ef2c88d6e
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-10-19 12:24:23 +1000

    Add basic word occurrences program to take a string and count occurrences of the words in it, to print the results

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,19 @@ Word Occurrences
 Estimate: 25 minutes
 Actual:
 """
+
+text = input("Enter a string: ")
+
+words = text.split()
+
+word_count = {}
+
+for word in words:
+    word = word.lower()
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+for word, count in word_count.items():
+    print(f"{word} : {count}")
