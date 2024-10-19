47cda2919fe25efa800b50c091fad95f0c14ffc0
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-10-19 12:50:30 +1000

    Add name extraction from email and display each extracted name for error checking

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,17 +5,29 @@ Actual:
 """
 
 def main():
+    """ Continuously prompts user for to enter an email address until a blank is entered """
+
     emails = []
 
     while True:
         email = input("Email: ")
         if email == "":
             break
+        name = extract_name_from_email(email)
+        print(f"Name: {name}")
         emails.append(email)
 
     print("Emails collected:")
     for email in emails:
         print(email)
+
+
+def extract_name_from_email(email):
+    """ Extracts name from provided email address, by splitting at @ and . or _ """
+
+    name_part = email.split("@")[0]
+    name = " ".join(part.title() for part in name_part.split("." or "_"))
+    return name
 
 
 main()
