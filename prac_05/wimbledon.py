02d6d5edd0bc5c70454cc63b2b3a1930df62c54e
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-10-19 13:49:07 +1000

    Load CSV file, process data into a dictionary of champions and their win counts

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,3 +3,31 @@ Wimbledon
 Estimate: 40 minutes
 Actual:
 """
+import csv
+
+def main():
+    filename = "wimbledon.csv"
+    data = load_data(filename)
+    champions = process_champions(data)
+    print(champions)
+
+
+def load_data(filename):
+    """ Loads CSV data from a file, skips the header row and returns as a list of rows """
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
+        data = list(reader)
+    return data
+
+
+def process_champions(data):
+    champions = {}
+    for row in data:
+        champion = row[2].strip()
+        if champion in champions:
+            champions[champion] += 1
+        else:
+            champions[champion] = 1
+    return champions
+
+main()
