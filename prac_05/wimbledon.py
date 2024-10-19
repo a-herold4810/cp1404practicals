f78953bf000e8f768c2852e9c1cd336e12827dbf
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-10-19 14:00:14 +1000

    Process set of countries that have had winners. Fix load_data to skip header row

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,18 +9,29 @@ def main():
     filename = "wimbledon.csv"
     data = load_data(filename)
     champions = process_champions(data)
-    print(champions)
+
+    print("Wimbledon Champions:")
+    for champion, wins in champions.items():
+        print(f"{champion} {wins}")
+
+    countries = process_countries(data)
+    sorted_countries = sorted(countries)
+    print(f"\nThese {len(sorted_countries)} countries have won Wimbledon:")
+    print(", ".join(sorted_countries))
 
 
 def load_data(filename):
     """ Loads CSV data from a file, skips the header row and returns as a list of rows """
+
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
-        data = list(reader)
+        data = list(reader)[1:]
     return data
 
 
 def process_champions(data):
+    """ Process the data to count how many times each champion has won """
+
     champions = {}
     for row in data:
         champion = row[2].strip()
@@ -29,5 +40,16 @@ def process_champions(data):
         else:
             champions[champion] = 1
     return champions
+
+
+def process_countries(data):
+    """ Process the data to extract the countries of the champions """
+
+    countries = set()
+    for row in data:
+        country = row[1].strip()
+        countries.add(country)
+    return countries
+
 
 main()
