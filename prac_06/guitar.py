f9919f0f8a7d311a09026148ab5fadfa3a9207ed
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-02 13:31:05 +1100

    Add original guitar, guitar test and guitars files

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+"""
+Guitar
+Estimate: 55 minutes
+Start Time: 1330
+Actual:
+Finish Time:
+"""
+
---
+++ prac_06/guitar_test.py
@@ -0,0 +1,7 @@
+"""
+Guitar Test
+Estimate: 55 minutes
+Start Time: 1330
+Actual:
+Finish Time:
+"""
