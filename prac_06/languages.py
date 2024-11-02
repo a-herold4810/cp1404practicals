21c6091fe47cba66f347f30d450823e4cb956a26
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-02 13:27:53 +1100

    Complete programs for languages and programming_language, built iteratively follow the prac

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,5 +2,26 @@
 Languages
 Estimate: 30 minutes
 Start Time: 1255
-Actual:
+Actual: 32 minutes
+Finish Time: 1327
 """
+
+from programming_language import ProgrammingLanguage
+
+# Sample instances for testing
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+main()
