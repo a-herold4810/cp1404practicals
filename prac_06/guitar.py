5deefdf9cc0ddf06f3f364a310afba9fdb9b387e
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-02 14:11:47 +1100

    Add program for Guitars! prac question

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,7 +2,31 @@
 Guitar
 Estimate: 55 minutes
 Start Time: 1330
-Actual:
-Finish Time:
+Actual: 40 minutes
+Finish Time: 1410
 """
 
+class Guitar:
+    """ Represent a Guitar object """
+
+    def __init__(self, name="", year=0, cost=0):
+        """ Initialise a Guitar instance with name, year, and cost attributes """
+
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """ Return a string representation of the Guitar object """
+
+        return f"{self.name} ({self.year}), worth ${self.cost:,.2f}"
+
+    def get_age(self):
+        """ Return the age of the guitar based on the current year """
+
+        current_year = 2024
+        return current_year - self.year
+
+    def is_vintage(self):
+        """ Return True if the guitar is 50 or more years old """
+        return self.get_age() >= 50
