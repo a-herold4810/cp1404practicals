66c7f1289446fdcba8cac6f5dec678a4ce77cfb9
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-08 20:32:56 +1100

    Complete modifications to walkthrough example

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,27 +7,34 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+                f", Pointer Arithmetic={self.pointer_arithmetic}")
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
+    def has_pointer_arithmetic(self):
+        """ Determine if language supports pointer arithmetic """
+        return self.pointer_arithmetic
+
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage(name="Ruby", typing="Dynamic", reflection=True, year=1995, pointer_arithmetic=False)
+    python = ProgrammingLanguage(name="Python", typing="Dynamic", reflection=True, year=1991, pointer_arithmetic=False)
+    visual_basic = ProgrammingLanguage(name="Visual Basic", typing="Static", reflection=False, year=1991,
+                                       pointer_arithmetic=False)
 
     languages = [ruby, python, visual_basic]
     print(python)
