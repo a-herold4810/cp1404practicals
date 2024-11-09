fb5d763b2850366168f0622aa4605bd2ddeca12b
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-10 08:27:12 +1100

    Add main menu options to display projects by completion status, filter by a certain date and relevant functions

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -6,6 +6,10 @@ Actual:
 Finish Time:
 """
 
+import csv
+import datetime
+from project import Project
+
 FILENAME = "projects.txt"
 
 
@@ -13,12 +17,36 @@ def main():
     """ Main program function to manage projects """
 
     projects = load_projects()
-    print("Projects loaded from file:")
-    display_projects(projects)
+
+    while True:
+        print("\nMenu:")
+        print("(D)isplay projects by completion")
+        print("(F)ilter projects by date")
+        print("(Q)uit")
+        choice = input(">>> ").lower()
+
+        if choice == 'd':
+            incomplete_projects = filter_projects_by_completion(projects, complete=False)
+            complete_projects = filter_projects_by_completion(projects, complete=True)
+            print("\nIncomplete projects:")
+            display_projects(incomplete_projects)
+            print("\nComplete projects:")
+            display_projects(complete_projects)
+        elif choice == 'f':
+            date_string = input("Show projects that start after date (dd/mm/yyyy): ")
+            date = datetime.datetime.strptime(date_string, "%d/%m/%Y")
+            filtered_projects = filter_projects_by_start_date(projects, date)
+            print("\nFiltered projects by start date:")
+            display_projects(filtered_projects)
+        elif choice == 'q':
+            break
+        else:
+            print("Invalid choice. Please choose a valid option.")
 
 
 def load_projects(filename=FILENAME):
-    """Load projects from a file and return a list of Project objects."""
+    """ Load projects from a file and return a list of Project objects"""
+
     projects = []
     with open(filename, "r") as file:
         file.readline()  # Skip header
@@ -29,9 +57,22 @@ def load_projects(filename=FILENAME):
 
 
 def display_projects(projects):
-    """Display projects in a readable format."""
+    """ Display projects in a readable format """
+
     for project in projects:
         print(project)
+
+
+def filter_projects_by_completion(projects, complete=True):
+    """ Filter projects by completion status """
+
+    return [project for project in projects if project.is_complete() == complete]
+
+
+def filter_projects_by_start_date(projects, date):
+    """ Filter projects that start after the specified date """
+
+    return [project for project in projects if project.starts_after(date)]
 
 
 main()
