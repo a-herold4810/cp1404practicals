e7e37388c351945de13f206ed81d8b9adc5badb0
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-10 07:33:43 +1100

    Add load_projects function and basic display function structure in project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -6,3 +6,32 @@ Actual:
 Finish Time:
 """
 
+FILENAME = "projects.txt"
+
+
+def main():
+    """ Main program function to manage projects """
+
+    projects = load_projects()
+    print("Projects loaded from file:")
+    display_projects(projects)
+
+
+def load_projects(filename=FILENAME):
+    """Load projects from a file and return a list of Project objects."""
+    projects = []
+    with open(filename, "r") as file:
+        file.readline()  # Skip header
+        for line in file:
+            name, start_date, priority, cost_estimate, percent_complete = line.strip().split('\t')
+            projects.append(Project(name, start_date, priority, cost_estimate, percent_complete))
+    return projects
+
+
+def display_projects(projects):
+    """Display projects in a readable format."""
+    for project in projects:
+        print(project)
+
+
+main()
