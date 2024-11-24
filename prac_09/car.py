4a698a4f4bfad05db0504944752eb19be3a9ae6f
a.herold4810 <angus.herold@my.jcu.edu.au>
2024-11-24 15:54:07 +1100

    Updated doc strings

--- prac_09/car.py
+++ prac_09/car.py
@@ -8,11 +8,7 @@ class Car:
     """Represent a Car object."""
 
     def __init__(self, name="", fuel=0):
-        """Initialise a Car instance.
-
-        name: string, reference name for car
-        fuel: float, one unit of fuel drives one kilometre
-        """
+        """ Initialise a Car instance """
         self.name = name
         self.fuel = fuel
         self.odometer = 0
@@ -26,11 +22,7 @@ class Car:
         self.fuel += amount
 
     def drive(self, distance):
-        """Drive the car a given distance.
-
-        Drive given distance if car has enough fuel
-        or drive until fuel runs out return the distance actually driven.
-        """
+        """Drive the car a given distance. """
         if distance > self.fuel:
             distance = self.fuel
             self.fuel = 0
